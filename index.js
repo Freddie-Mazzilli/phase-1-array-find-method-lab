9870d5d0f4461c4f8e4269561cbcb0a9b5b6cec5
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-04-14 11:07:18 -0400

    Completes array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+
+function superbowlWin(record){
+    const win = record.find((record) => record.result === 'W')
+    if (typeof win === "object"){
+        return win.year
+    } else {return}
+}
